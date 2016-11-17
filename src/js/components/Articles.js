f9df0c55a02c6f523b3179de7681f38dd1bdc088
Yurii Vydai <djurker21@gmail.com>
2016-11-17 14:47:27 +0200

    Test2 articles

--- src/js/components/Articles.js
+++ src/js/components/Articles.js
@@ -1,22 +1,45 @@
 import React, { Component } from 'react';
+import { Link, browserHistory } from 'react-router'
 import { inject, observer } from 'mobx-react';
 
 @inject('appState')
 @observer
-class Articles extends Component {
+export default class Articles extends Component {
+  componentDidMount() {
+    this.props.appState.loadArticles()
+  }
+
+  // Render list of Articles
+  renderArticles(articles) {
+    return articles.map((article) => {
+      return (
+        <tr key={ article.id }>
+          <td>{ article.id }</td>
+          <td>{ article.title }</td>
+          <td>
+          </td>
+        </tr>
+      )
+    })
+  }
+
   render() {
     return (
-      <div>
-        <button onClick={this.onReset}>
-          Seconds passed: {this.props.appState.timer}
-        </button>
+      <div className='container'>
+        <h1>Articles</h1>
+        <table>
+          <thead>
+            <tr>
+              <td>ID</td>
+              <td>Title</td>
+              <td>Actions</td>
+            </tr>
+          </thead>
+          <tbody>
+            { this.renderArticles(this.props.appState.articles) }
+          </tbody>
+        </table>
       </div>
-    );
+    )
   }
-
-  onReset = () => {
-    this.props.appState.resetTimer();
-  }
-};
-
-export default Articles;
+}
--- src/js/stores/AppState.js
+++ src/js/stores/AppState.js
@@ -1,15 +1,17 @@
-import { observable } from 'mobx';
+import { observable, action } from 'mobx';
+import axios from 'axios'
+
+const API_URL = 'https://serene-ocean-15499.herokuapp.com/api/v1'
 
 export default class AppState {
-  @observable timer = 0;
+  @observable articles = []
 
-  constructor() {
-    setInterval(() => {
-      this.timer += 1;
-    }, 1000);
-  }
-
-  resetTimer() {
-    this.timer = 0;
+  @action loadArticles() {
+    let request = axios({
+      method: 'get',
+      url: `${API_URL}/articles`,
+      headers: []
+    })
+    this.articles = request.data
   }
 }
