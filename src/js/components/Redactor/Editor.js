98b0cd2e91b41d892076f4526ad878b76e7cdcd9
Yurii Vydai <djurker21@gmail.com>
2016-11-22 11:44:18 +0200

    Added textarea autosize and fixed submit button position

--- src/js/components/Redactor/Editor.js
+++ src/js/components/Redactor/Editor.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Textarea from 'react-textarea-autosize'
 
 export default class Editor extends React.Component {
   render() {
     return (
       <div className={ this.props.showType }>
         <h4 className='center'>Editor</h4>
-        <textarea value={ this.props.text } onChange={ this.props.onChange } ></textarea>
+        <Textarea value={ this.props.text } onChange={ this.props.onChange } rows='3'></Textarea>
       </div>
     )
   }
