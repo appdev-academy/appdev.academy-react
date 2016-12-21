599aa76c68d1a4414c3905a67c97e02c587b5097
Maksym Skliarov <maksym.skliarov@gmail.com>
2016-12-21 22:19:50 +0200

    Show author, published_at and updated_ar in frontend

--- src/js/components/Frontend/Articles/Show.js
+++ src/js/components/Frontend/Articles/Show.js
@@ -16,9 +16,15 @@ export default class Show extends React.Component {
   
   render() {
     let article = this.props.articlesStore.article
+    let authorName = ''
+    if (article.author) {
+      authorName = article.author.full_name
+    }
     return (
       <div className='article-container'>
         <h2 className='center'>{ article.title }</h2>
+        <div>Published by { authorName } on { article.published_at }</div>
+        <div>last update on { article.updated_at }</div>
         <div dangerouslySetInnerHTML={{ __html: article.html_content }} />
       </div>
     )
