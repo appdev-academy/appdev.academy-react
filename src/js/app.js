747a11dd42d651dc052679b48d321b24752639f1
Maksym Skliarov <maksym.skliarov@gmail.com>
2017-01-03 12:18:18 +0200

    Added Google Analytics

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga'
 import { Router, browserHistory } from 'react-router'
 import { Provider } from 'mobx-react'
 
@@ -16,9 +17,16 @@ const articlesStore = new ArticlesStore(sessionsStore)
 const articleImagesStore = new ArticleImagesStore(sessionsStore)
 const pagesStore = new PagesStore(sessionsStore)
 
+// Initialize Google Analytics
+ReactGA.initialize('UA-46802250-3')
+
+function trackPageview() {
+  ReactGA.pageview(window.location.pathname)
+}
+
 ReactDOM.render(
   <Provider articlesStore={ articlesStore } articleImagesStore={ articleImagesStore } pagesStore={ pagesStore } sessionsStore={ sessionsStore }>
-    <Router history={ browserHistory } routes={ routes } />
+    <Router history={ browserHistory } routes={ routes } onUpdate={ trackPageview } />
   </Provider>,
   document.getElementById('body')
 )
