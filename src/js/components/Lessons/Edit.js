781b49b620d62a99c3a1ea19649ba36532ba87fa
Maksym Skliarov <maksym.skliarov@gmail.com>
2017-05-21 14:44:37 +0300

    Errors list for Lessons

--- src/js/components/Lessons/Edit.js
+++ src/js/components/Lessons/Edit.js
@@ -8,6 +8,13 @@ import Form from './Form'
 @observer
 export default class Edit extends React.Component {
   
+  constructor(props) {
+    super(props)
+    this.state = {
+      errors: []
+    }
+  }
+  
   componentDidMount() {
     let lessonID = this.props.params.lessonID
     let lessonForm = this.refs.lessonForm
@@ -27,12 +34,19 @@ export default class Edit extends React.Component {
       if (response.status == 200) {
         browserHistory.push(`/topics/${topicID}/screencasts/${screencastID}/lessons`)
       }
+    }).catch((error) => {
+      if (error.response && error.response.data && error.response.data.errors) {
+        this.setState({
+          errors: error.response.data.errors
+        })
+      }
     })
   }
   
   render() {
     return (
       <Form
+        errors={ this.state.errors }
         params={ this.props.params }
         handleSubmit={ this.handleSubmit.bind(this) } ref='lessonForm'
       />
--- src/js/components/Lessons/New.js
+++ src/js/components/Lessons/New.js
@@ -7,6 +7,13 @@ import Form from './Form'
 @inject('lessonsStore')
 export default class New extends React.Component {
   
+  constructor(props) {
+    super(props)
+    this.state = {
+      errors: []
+    }
+  }
+  
   handleSubmit(screencastParams) {
     let topicID = this.props.params.topicID
     let screencastID = this.props.params.screencastID
@@ -15,12 +22,19 @@ export default class New extends React.Component {
       if (response.status == 200) {
         browserHistory.push(`/topics/${topicID}/screencasts/${screencastID}/lessons`)
       }
+    }).catch((error) => {
+      if (error.response && error.response.data && error.response.data.errors) {
+        this.setState({
+          errors: error.response.data.errors
+        })
+      }
     })
   }
   
   render() {
     return (
       <Form
+        errors={ this.state.errors }
         params={ this.props.params }
         handleSubmit={ this.handleSubmit.bind(this) }
       />
